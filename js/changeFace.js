202fbb16ee91680c94919c6814f6b4096c19c0e5
HYEOK999 <iasg2004@naver.com>
2019-11-18 06:13:22 +0900

    feat : Add the Happy face feat 0. change the expression 1. repeat the heart to top 2. Add the animation heart 3. Add the js ( Change the expression )
    	modified:   css/style.css
    	modified:   index.html
    	modified:   js/changeFace.js
    	new file:   js/heart.js

--- js/changeFace.js
+++ js/changeFace.js
@@ -1,18 +1,36 @@
-let gitCount = 2;
+let gitCount = 4;
 const $moominForm = document.querySelector('.moomin-form');
-const $vision = document.querySelector('.vision');
+const $normalEye = document.querySelector('.normal-eye');
 const $angryEye = document.querySelector('.angry-eye');
 const $angryAdd = document.querySelector('.angry-add');
 const $angryMark = document.querySelectorAll('.angry-mark');
+const $happyEye = document.querySelector('.happy-eye');
+const $happyHearts = document.querySelector('.happy-hearts');
+
 
 $moominForm.onclick = () => {
-  gitCount = 4;
-  console.dir($angryMark);
-  if (gitCount == 4) {
-    $vision.style.display = 'none';
+if (gitCount == 2) {
+    console.dir($angryMark);
+    $normalEye.style.display = 'none';
+    $happyEye.style.display = 'none';
+    $happyHearts.style.display = 'none';
     $angryEye.style.display = 'block';
     $angryAdd.classList.add('angry-div');
     $angryMark[0].style.display = 'block';
     $angryMark[1].style.display = 'block';
+} else if (gitCount == 3) {
+  $happyEye.style.display = 'none';
+  $happyHearts.style.display = 'none';
+  $angryEye.style.display = 'none';
+  $angryMark[0].style.display = 'none';
+  $angryMark[1].style.display = 'none';
+  $normalEye.style.display = 'block';
+} else {
+    $angryEye.style.display = 'none';
+    $normalEye.style.display = 'none';
+    $angryMark[0].style.display = 'none';
+    $angryMark[1].style.display = 'none';
+    $happyEye.style.display = 'block';
+    $happyHearts.style.display = 'block';
   }
 }
